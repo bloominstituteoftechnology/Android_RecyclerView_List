e70c8002fdda85160e184cd24e0ff6e526d9e2a0
BL-CoderOvertime <brandonlively@live.com>
2019-03-26 17:36:42 -0400

    set-up recyclerView skeleton

--- app/src/main/java/com/example/imageviewer/ImageListAdapter.java
+++ app/src/main/java/com/example/imageviewer/ImageListAdapter.java
@@ -1,4 +1,33 @@
 package com.example.imageviewer;
 
-public class ImageListAdapter {
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ImageListAdapter extends RecyclerView.Adapter<ImageListAdapter.ViewHolder>{
+
+    class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+        }
+    }
+
+    @NonNull
+    @Override
+    public ImageListAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ImageListAdapter.ViewHolder viewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 }
